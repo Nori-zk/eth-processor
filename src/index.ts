50b3fee55844d6fbbb91c5a1321ddf5dddaabf3f
Jonathan Kelsey <jonathan.s.kelsey@gmail.com>
2025-02-26 17:09:58 +0000

    fix(codebase): changing misspelling of submitter in export

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 import { EthVerifierComputeOutput } from './interfaces.js';
 import { MinaEthProcessorSubmitter } from './proofSubmitter.js';
-export { EthVerifierComputeOutput, MinaEthProcessorSubmitter as MinaEthProcessorSubmittor };
+export { EthVerifierComputeOutput, MinaEthProcessorSubmitter };
