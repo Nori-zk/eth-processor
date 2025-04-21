95e7e2bfc5f0f5e513fe83c1f8b165a9d8feb89c
Jonathan Kelsey <jonathan.s.kelsey@gmail.com>
2025-04-21 23:03:12 +0100

    fix(tests): wait on tx id not hash

--- src/proofSubmitter.spec.ts
+++ src/proofSubmitter.spec.ts
@@ -78,34 +78,9 @@ describe('MinaEthProcessorSubmittor Integration Test', () => {
             logger.log(`txHash: ${result.txHash}`);
 
             // Wait for finalization
-            await wait(result.txHash, process.env.MINA_RPC_NETWORK_URL!);
-            await new Promise((res) => setTimeout(res, 1000)); // Sleep a bit because in test mode wait doesn't really stall
+            await wait(result.txId, process.env.MINA_RPC_NETWORK_URL!);
+
             i++;
         }
-    });
-
-    test('custom wait should await finalisation successfully', async () => {
-        // Construct a MinaEthProcessorSubmittor
-        const proofSubmitter = new MinaEthProcessorSubmitter();
-
-        // Establish the network
-        await proofSubmitter.networkSetUp();
-
-        // Compile contracts.
-        await proofSubmitter.compileContracts();
-        // If local deploy contracts.
-        if (proofSubmitter.testMode === true) {
-            await proofSubmitter.deployContract();
-        }
-        // Build proof.
-        const ethProof = await proofSubmitter.createProof(
-            buildExampleProofCreateArgument()
-        );
-
-        // Submit proof.
-        const result = await proofSubmitter.submit(ethProof.proof);
-
-        // Wait for finalization
-        await wait(result.txId, process.env.MINA_RPC_NETWORK_URL!);
     });
 });
